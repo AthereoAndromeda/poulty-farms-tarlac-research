3dd9647ac4bd248d5dfed19d17002d0b0e35fd36
Athereo <athereoandromeda@gmail.com>
2024-07-30 17:22:18 +0800

    nix: Update Nix Flakes to use flake-utils

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, ... }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
-    in
-    {
-      devShells = forAllSystems (system: {
-        default = nixpkgs.legacyPackages.${system}.callPackage ./shell.nix { };
-      });
-    };
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem(system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells = {
+          default = pkgs.callPackage ./shell.nix { };
+        };
+      }
+    );
 }
--- shell.nix
+++ shell.nix
@@ -8,16 +8,20 @@ pkgs.mkShell {
     pythonPackages.python
     pythonPackages.venvShellHook
   ];
+
   packages = [ pkgs.poetry ];
+
   venvDir = "./.venv";
+
   postVenvCreation = ''
     unset SOURCE_DATE_EPOCH
     poetry env use .venv/bin/python
     poetry install
   '';
+
   postShellHook = ''
     unset SOURCE_DATE_EPOCH
-    export LD_LIBRARY_PATH=${lib.makeLibraryPath [stdenv.cc.cc]}
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${lib.makeLibraryPath [stdenv.cc.cc]}
     poetry env info
   '';
 }
